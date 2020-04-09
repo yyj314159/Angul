b2d6285b72b43796dfdee8796de379c8c6ff75f5
chefyang <1936579718@qq.com>
2020-04-09 21:31:33 +0800

    脚手架

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
+
+declare var module: any;
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if ((environment as any).hmr) {
+  if (module.hot) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-job!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    bootstrap().catch(err => console.log(err));
+  });
+}
